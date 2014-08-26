52358e908567d4335545a6917ddcd4d96e96bb08
Alexandr Ivanov <al_i_88@mail.ru>
2014-08-26 16:08:49 +0800

    myMenu update

--- protected/widgets/MyMenu.php
+++ protected/widgets/MyMenu.php
@@ -10,7 +10,8 @@ class MyMenu extends CMenu{
 			if(!isset($v[2]) or $v[2]==true)
 				$result[]='<div class="item">'.CHtml::link($v[0],array($v[1])).'</div>';
 		}
-		return '<div class="MyMenuSubmenu">'.implode('<div class="limiter"></div>',$result).'</div>';
+		if(!empty($result))
+			return '<div class="MyMenuSubmenu">'.implode('<div class="limiter"></div>',$result).'</div>';
 		
 	}
 	protected function renderMenuItem($item)
@@ -21,10 +22,61 @@ class MyMenu extends CMenu{
 		if(isset($item['url']))
 		{
 			$label=$this->linkLabelWrapper===null ? $item['label'] : CHtml::tag($this->linkLabelWrapper, $this->linkLabelWrapperHtmlOptions, $item['label']);
-			return CHtml::link($label,$item['url'],isset($item['linkOptions']) ? $item['linkOptions'] : array()).$submenu;
+			return array(CHtml::link($label,$item['url'],isset($item['linkOptions']) ? $item['linkOptions'] : array()),$submenu);
 		}
 		else
-			return CHtml::tag('span',isset($item['linkOptions']) ? $item['linkOptions'] : array(), $item['label']);
+			return array(CHtml::tag('span',isset($item['linkOptions']) ? $item['linkOptions'] : array(), $item['label']),$submenu);
+	}
+
+
+	protected function renderMenuRecursive($items)
+	{
+		$count=0;
+		$n=count($items);
+		foreach($items as $item)
+		{
+			$count++;
+			$options=isset($item['itemOptions']) ? $item['itemOptions'] : array();
+			$class=array();
+			if($item['active'] && $this->activeCssClass!='')
+				$class[]=$this->activeCssClass;
+			if($count===1 && $this->firstItemCssClass!==null)
+				$class[]=$this->firstItemCssClass;
+			if($count===$n && $this->lastItemCssClass!==null)
+				$class[]=$this->lastItemCssClass;
+			if($this->itemCssClass!==null)
+				$class[]=$this->itemCssClass;
+			if($class!==array())
+			{
+				if(empty($options['class']))
+					$options['class']=implode(' ',$class);
+				else
+					$options['class'].=' '.implode(' ',$class);
+			}
+
+			echo CHtml::openTag('li', $options);
+
+			$res=$this->renderMenuItem($item);
+			$menu=$res[0];
+			$subm=$res[1];
+			if(isset($this->itemTemplate) || isset($item['template']))
+			{
+				$template=isset($item['template']) ? $item['template'] : $this->itemTemplate;
+				echo strtr($template,array('{menu}'=>$menu));
+			}
+			else
+				echo $menu;
+
+			if(isset($item['items']) && count($item['items']))
+			{
+				echo "\n".CHtml::openTag('ul',isset($item['submenuOptions']) ? $item['submenuOptions'] : $this->submenuHtmlOptions)."\n";
+				$this->renderMenuRecursive($item['items']);
+				echo CHtml::closeTag('ul')."\n";
+			}
+
+			echo CHtml::closeTag('li')."\n";
+			echo $subm;
+		}
 	}
 }
 	?>
