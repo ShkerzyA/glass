e9c8fd08e2be3fc72599a2324064610f3e57e474
Alexandr Ivanov <al_i_88@mail.ru>
2014-09-24 15:24:28 +0800

    eq mod

--- protected/models/Equipment.php
+++ protected/models/Equipment.php
@@ -117,6 +117,7 @@ class Equipment extends CActiveRecord
 				34=>'Allied Telesis',
 				35=>'DNS',
 				36=>'Unitech',
+				37=>'Wide',
 				99=>'Другое',
 
 				)),
@@ -158,6 +159,7 @@ class Equipment extends CActiveRecord
 				34=>array('class'=>'c8'),
 				35=>array('class'=>'c0 c1'),
 				36=>array('class'=>'c15'),
+				37=>array('class'=>'c1'),
 				99=>array('class'=>'c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11 c12 c13 c14 c15 c99'),
 				)
 			),
