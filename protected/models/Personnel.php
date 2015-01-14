d1906fa8801657fef21730e264d04529a2942107
Alexandr Ivanov <al_i_88@mail.ru>
2015-01-14 11:24:30 +0700

    fix wrong pass with wrong method

--- protected/models/Personnel.php
+++ protected/models/Personnel.php
@@ -106,7 +106,8 @@ class Personnel extends CActiveRecord
         $symbols=array('а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я');
         $replace = array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z');
 
-        $str=mb_strtolower($this->surname.$this->name.$this->patr.$this->birthday.'scotland');
+        $str=mb_strtolower($this->surname.$this->name.$this->patr.$this->birthday,'binary').'scotland';
+        //$res=$str;
         $res=substr(md5($str),0,3);
 
 
