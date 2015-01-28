f5a3f4fcf6560a4bc3dfff0575755ad80b18d9fc
Alexandr Ivanov <al_i_88@mail.ru>
2015-01-28 10:52:23 +0700

    MU.fdb tree2

--- protected/views/department/mufdb.php
+++ protected/views/department/mufdb.php
@@ -30,7 +30,7 @@ $root=array('ID_MU0'=>'1712','IDMU'=>'60','IDPARENT'=>'');
 
 $lvl='';
  function getTree($item,$result){
- 		echo '<div style="border-left: 2px solid black; border-bottom: 2px solid black; margin: 10px; margin-left: 30px;">'.$result['mu'][$item['IDMU']]['MU'].'<br>';
+ 		echo '<div style="border-left: 2px solid black; border-bottom: 2px solid black; margin: 10px; margin-left: 30px;">'.$result['mu'][$item['IDMU']]['MU'].'('.$item['IDMU'].')';
  		$child=array_filter($result['mu0'], function($var) use ($item){return ($var['IDPARENT']==$item['ID_MU0']);});
  		//print_r($child);
  		if(!empty($child)){
