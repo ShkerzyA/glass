177c94058a94f04f8df1783542e247fdcc68d3ed
Alexandr Ivanov <al_i_88@mail.ru>
2017-11-07 12:33:42 +0700

    command, quickq,spg,qms import

--- protected/commands/PersProgramCommand.php
+++ protected/commands/PersProgramCommand.php
@@ -5,21 +5,56 @@ class PersProgramCommand extends CConsoleCommand
 		$xls=new Xls();
 		$fileXls=$xls->load($filepath,True);
 		//print_r($fileXls);
+		$now=strtotime("now");
 		foreach ($fileXls as $persArr) {
-			if(!empty($persArr[2])){
-				$fio=explode(' ',$persArr[2]);
+			if(!empty($persArr[3])){
+				$fio=explode(' ',$persArr[3]);
+				if(empty($fio[2]))
+					continue;
+
+				if ($persArr[7]!=1 or (!empty($persArr[6]) and ($now>strtotime($persArr[6]))))
+					continue;
+				//print_r($persArr);
 				if($pers=Personnel::model()->find(array('condition'=>'t.surname=\''.$fio[0].'\' and t.name=\''.$fio[1].'\' and t.patr=\''.$fio[2].'\''))){
 					$pp=new PersProgram;
-					$pp->attributes=array('id_pers'=>$pers->id,'id_program'=>2,'login'=>$persArr[1]);
+					$pp->attributes=array('id_pers'=>$pers->id,'id_program'=>2,'login'=>$persArr[2]);
 					if($pp->validate()){
 						try{
 							$pp->save();
 						} catch (Exception $e) {
-							echo ($persArr[2]." ".$e->getMessage()."\n\n");  
+							echo ($persArr[3]." ".$e->getMessage()."\n\n");  
 						}
 					}
 				}
 			}
+		}
+		echo "\nend\n";
+	}
+
+	public function actionFromXlsQuickq($filepath) {
+		$xls=new Xls();
+		$fileXls=$xls->load($filepath,True);
+		print_r($fileXls);
+		die();
+		foreach ($fileXls as $persArr) {
+			if(empty($persArr[1]) or empty($persArr[2]) or empty($persArr[3]) ){
+				continue;
+			}
+			if($pers=Personnel::model()->find(array('condition'=>'t.surname=\''.$persArr[1].'\' and t.name=\''.$persArr[2].'\' and t.patr=\''.$persArr[3].'\''))){
+				$pp=new PersProgram;
+				$pp->attributes=array('id_pers'=>$pers->id,'id_program'=>2,'login'=>$persArr[0]);
+				if($pp->validate()){
+					print_r($pp->attributes);
+					/*
+					try{
+						$pp->save();
+					} catch (Exception $e) {
+						echo ($persArr[2]." ".$e->getMessage()."\n\n");  
+					}
+					*/
+				}
+			}
+			
 		}
 		echo "\nend\n";
 	}
