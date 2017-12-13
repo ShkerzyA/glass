0e7bbdf3251618c049489aebc69f856cc84f05fe
Alexandr Ivanov <al_i_88@mail.ru>
2017-12-13 13:19:21 +0700

    try4

--- protected/commands/PersProgramCommand.php
+++ protected/commands/PersProgramCommand.php
@@ -6,7 +6,7 @@ class PersProgramCommand extends CConsoleCommand
 		$fileXls=$xls->load($filepath,True);
 		#$fileXls=array();
 		//print_r($fileXls);
-		spl_autoload_unregister(array('YiiBase','autoload'));
+		spl_autoload_register(array('YiiBase','autoload'));
 		if($delete)
 			PersProgram::model()->deleteAll(array('condition'=>'id_program=2'));
 		$now=strtotime("now");
